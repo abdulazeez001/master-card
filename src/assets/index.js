9750894ae3d160edae44cf96d5bdfceab1a6d602
Abdulazeez Shittu <41093124+abdulazeez001@users.noreply.github.com>
2021-12-09 02:00:04 +0100

    Add files via upload

---
+++ src/assets/index.js
@@ -0,0 +1,14 @@
+import DiscoverBrand from './images/cc-discover-brands.svg';
+import PaypalBrand from './images/cc-paypal-brands.svg';
+import VisaBrand from './images/cc-visa-brands.svg';
+import Master from './images/master.jpeg';
+import User from './images/user-circle-regular.svg';
+
+
+export {
+    DiscoverBrand,
+    PaypalBrand,
+    VisaBrand,
+    Master,
+    User
+}
